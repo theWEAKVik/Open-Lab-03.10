a3a52bc7c01444608fb9419f05a8c7b27940231b
Viktor <krpectv57@gmail.com>
2020-10-06 20:11:04 +0200

    proste to nejak ide

--- Open-Lab-03.10/Checker.cs
+++ Open-Lab-03.10/Checker.cs
@@ -6,7 +6,9 @@ namespace Open_Lab_03._10
     {
         public int GetNumberOfCharsInString(char letter, string str)
         {
-            throw new NotImplementedException();
+            int freq = str.Split(letter).Length - 1;
+            return freq;
+            
         }
     }
 }
